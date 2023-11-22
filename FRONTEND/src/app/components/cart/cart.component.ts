9f2f26878b50b37ffec1249442b547c76437e8f5
Maël Ehrhard <mael.ehrhard@outlook.fr>
2023-11-22 17:14:37 +0100

    ✨ Add remove from cart

--- FRONTEND/src/app/components/cart/cart.component.ts
+++ FRONTEND/src/app/components/cart/cart.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Product} from "../../shared/models/product.model";
-import {Select} from "@ngxs/store";
+import {Select, Store} from "@ngxs/store";
 import {map, Observable} from "rxjs";
 import {ProductState} from "../../shared/states/product.state";
+import {RemoveProduct} from "../../shared/actions/product.actions";
 
 @Component({
   selector: 'app-cart',
@@ -10,13 +11,18 @@ import {ProductState} from "../../shared/states/product.state";
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit{
-  constructor() {
+  constructor(private readonly store: Store) {
   }
+
   @Select(ProductState.getProducts) addedProducts$: Observable<Product[]>;
   productNumber$: Observable<number>;
 
   ngOnInit(): void {
     this.productNumber$ = this.addedProducts$.pipe(
       map(products => products.length));
+  }
+
+  onProductDelete(p: Product) {
+    this.store.dispatch(new RemoveProduct(p));
   }
 }
